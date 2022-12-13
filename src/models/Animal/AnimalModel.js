dc32aae0cae8d03cf5722a77a9b646b51e09bc0f
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-12-13 16:12:20 +1100

    Add & use Schema for Animal

--- src/models/Animal/AnimalModel.js
+++ src/models/Animal/AnimalModel.js
@@ -1,11 +1,14 @@
 const { moduleExpression } = require("@babel/types");
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const Animal = mongoose.model("Animal", {
+const animalSchema = new Schema({
   name: String,
   species: String,
   age: Number,
   colour: String,
 });
+
+const Animal = mongoose.model("Animal", animalSchema);
 
 module.exports = { Animal };
